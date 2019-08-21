aff2ac969147bc4a4718a24ee2b1d6ccafef87e5
Erin Johnson <erinjohnson47@gmail.com>
2019-08-21 14:37:20 -0700

    user login route

--- api/user.py
+++ api/user.py
@@ -37,16 +37,11 @@ def register():
     payload = request.form.to_dict()
     dict_file = pay_file.to_dict()
     print(payload, 'payload', type(payload), 'type')
-    
-
-
-    print(payload)
-    print(dict_file)
 
     payload['email'].lower()
     try:
         models.User.get(models.User.email == payload['email'])
-        return jsonify(data={}, status={"code": 401, "message": "A user with that name or email exists"})
+        return jsonify(data={}, status={"code": 401, "message": "A user with that username or email exists"})
     except models.DoesNotExist: 
         payload['password'] = generate_password_hash(payload['password'])
         file_picture_path = save_picture(dict_file['file'])
@@ -62,23 +57,22 @@ def register():
         print(user_dict)
         print(type(user_dict))
 
-
         del user_dict['password']
 
         return jsonify(data=user_dict, status={"code": 201, "message": "Success"})
 
 
-
 @user.route('/login', methods=["POST"])
 def login():
-    payload = request.form.to_dict()
+    payload = request.get_json()
     print(payload, '<-payload')
     try:
         user = models.User.get(models.User.username == payload['username'])
         user_dict = model_to_dict(user)
+        print(user_dict, 'user_dict in login route')
         if(check_password_hash(user_dict['password'], payload['password'])):
-            del user_dict['password']
             login_user(user)
+            del user_dict['password']
             print(current_user, '<-current user')
             print(user_dict, '<-user_dict')
             return jsonify(data=user_dict, status={"code": 200, "message": "Success"})
